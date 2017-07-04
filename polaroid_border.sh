2b0ec5ae8e97f83f5751ee305a1e447cb5fa4d6b
travis <travis@sebastian.science>
2017-07-04 15:04:37 +0000

    Built page by Travis CI

---
+++ polaroid_border.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+convert wizard: -font FrenteH1-Regular -background black -gravity SouthWest -fill rgba(255,255,255,0.92) -splice 0x32 -pointsize 16 -annotate 0x0 'ART' -splice 0x24 -pointsize 16 -annotate 0x0 'capsicum pubescens' -splice 0x24 -annotate 0x0 'TYP' -splice 0x24 -annotate 0x0 'Rocoto Brown' -bordercolor black -border 16x16 rose.jpg
